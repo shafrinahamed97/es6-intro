42528aa632d5c6d6de31c1852ebc8a730991093b
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-25 14:19:55 +0600

    Added new frined using js and added bg

--- js/dom.js
+++ js/dom.js
@@ -4,9 +4,25 @@ const friends = document.getElementsByClassName('frined');
 for(const frined of friends){
   frined.style.backgroundColor = 'lightblue';
 }
-} )
+} );
 
 document.getElementById('center-third').addEventListener('click',function(){
     const third  = document.getElementById('third-frined');
     third.style.textAlign = 'center';
+});
+
+document.getElementById('add-frined').addEventListener('click', function(){
+    const friendContainer = document.getElementById('frineds');
+    const friend = document.createElement('div');
+    friend.classList.add('frined');
+    friend.innerHTML = `
+    <h3 class="friend-name ">New Frined</h3>
+    <p> Something new added</p>
+    `
+
+    friendContainer.appendChild(friend);
+    
+    
+    
+
 })
