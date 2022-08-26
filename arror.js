5770922ceba58fbea5f871ea9d004406f149483c
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-26 11:33:38 +0600

    added a interview question

--- arror.js
+++ arror.js
@@ -1,38 +1,43 @@
 // function declaration
-function add(first, second)
+// function add(first, second)
 // {
 //     const total = first+second;
 //     return total;
 // }
 
 // function expression
-const add1 = function add1(first,second)
-{
-    const total = first+second;
-    return total;
-}
- const result = add1(10,20);
- console.log(result);
+// const add1 = function add1(first,second)
+// {
+//     const total = first+second;
+//     return total;
+// }
+//  const result = add1(10,20);
+//  console.log(result);
 
  // function expression with anonymous function
- const add2 = function(first,second){
-    const total = first+second;
-    return total;
- }
+// //  const add2 = function(first,second){
+// //     const total = first+second;
+// //     return total;
+//  }
  
-  const result2 = add(41,58);
-  console.log(result2);
+//   const result2 = add(41,58);
+//   console.log(result2);
 
 
 
-function add4(first, second){
-    return first+second;
-}
+// function add4(first, second){
+//     return first+second;
+// }
 
-const add5 = function(first,second){
-    return first+second;
- }
- 
+// const add5 = function(first,second){
+//     return first+second;
+//  }
+
+// arrow function
  const add6 = (first,second) => first+second;
- const result3 = add(10,20);
+
+
+ const result3 = add6(30,30);
  console.log(result3);
+
+ // google it=> interview question: differences between function declaration, function expression and arrow function.
