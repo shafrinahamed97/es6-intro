bd191e418d64813e8171970deccf09253ba926f8
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-26 11:46:24 +0600

    more arrow.js

---
+++ more-arrow.js
@@ -0,0 +1,19 @@
+const add = (first, second) => first+second;
+const getFullName = (first, last)=> first + ' ' + last;
+const multiply = (a,b) => a*b;
+
+const result = multiply(7,8);
+// console.log(result);
+
+const addAll = (a,b,c,d,e,f)=> a+b+c+d+e+f;
+
+// No parameter arrow function
+const getPie = () => 3.14
+
+// one parameter
+const doubleIt = (num) => num*2;
+
+// one paramenter simple version
+const fiveTimes = num => num *5;
+
+
