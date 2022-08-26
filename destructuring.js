5bf4d2da3bb523fae4a04ee4d91fa9ef83f730d0
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-26 14:12:46 +0600

    last'er vdo dekhte hobe

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+const fis = {
+    name:'king Hilsa',
+    address:'chandput',
+    color: 'silver',
+    phone: '018',
+    price: 4000
+}
+// console.log(fis.phone);
+const phone = fis.phone;
+// console.log(phone);
+console.log(phone);
+console.log(phone);
+
