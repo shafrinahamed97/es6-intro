668e26dfa1328500837efeb78464d147c22003f8
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-26 13:48:37 +0600

    added push

--- spread.js
+++ spread.js
@@ -3,6 +3,12 @@ const max= Math.max(12,85,99,78);
 // console.log(max);
 
  const numbers = [23,96,99,78];
- const largest = Math.max(numbers);
- console.log(...numbers);
+ const largest = Math.max(...numbers);
+//  console.log(...numbers);
+//  console.log(largest);
+
+const numbers2 = numbers;
+numbers.push(26);
+console.log(numbers);
+console.log(numbers2);
  
