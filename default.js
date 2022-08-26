15be6870249fb2cc5c47ad0bf667fbdce619c3df
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-26 10:20:04 +0600

    using backtick ( )

--- default.js
+++ default.js
@@ -6,10 +6,13 @@
 // const result = add();
 // console.log(result);
 
-function add(first, second =0)
+function add(first = 0, second =0)
 {
     const total = first+second;
     return total;
 }
- const result = add(10);
+ const result = add(10,10);
  console.log(result);
+
+ // DEFAULT PARAMETERS
+ // Default function parameters allow namned parameters to be initialized with default values if no value or undefined is passed
---
+++ template.js
@@ -0,0 +1,18 @@
+// Template string, multiple line string, dynamic string
+
+const person = 'Shafrin';
+const person2 = 'ahamed';
+const person3 = `Shakib`;
+
+// previous system 
+const multiline = 'First Line text \n' + 'second line of code\n' + 'third line of text\n' + 'Fourth line of string';
+// console.log(multiline);
+
+
+// Present system using backtick(` `)
+const newMultiline = `First line of text
+Secont line of tecxt
+third line of code
+fourth line of code`;
+
+console.log(newMultiline);
