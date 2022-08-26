1a9096d6fee8a3a879f7117cc891bc26e9b14eba
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-26 14:00:11 +0600

    added comments

--- spread.js
+++ spread.js
@@ -7,8 +7,15 @@ const max= Math.max(12,85,99,78);
 //  console.log(...numbers);
 //  console.log(largest);
 
-const numbers2 = numbers;
+const numbers2 = [...numbers];
 numbers.push(26);
+numbers2.push(777);
 console.log(numbers);
 console.log(numbers2);
  
+const numbers4 = [444, 78, ...numbers,111,22]
+/*
+        SPEREAD OPERATION: The JavaScript spread operator(... ) allows us quickly copy all or part of an existing array or object into other array or object.
+   
+
+*/
